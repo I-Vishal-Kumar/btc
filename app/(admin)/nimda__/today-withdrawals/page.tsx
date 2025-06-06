2ef41f7768112d54f6664a3700602046e9ecd6f4
I-Vishal-kumar <vishal2983kumar@gmail.com>
2025-06-06 13:20:45 +0530

    add force dynamic

--- app/(admin)/nimda__/today-withdrawals/page.tsx
+++ app/(admin)/nimda__/today-withdrawals/page.tsx
@@ -1,3 +1,6 @@
 import { TodayWithdrawals } from "@/app/__admin_components__/todayWithdrawals";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 43200;
+
 export default TodayWithdrawals
