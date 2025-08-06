493d6e6663324403d8ed3765143d81ebe2fe8d07
I-Vishal-kumar <vishal2983kumar@gmail.com>
2025-08-06 19:47:25 +0530

    change number fix tax

--- app/api/payment/AUTO_WITHDRAW/route.ts
+++ app/api/payment/AUTO_WITHDRAW/route.ts
@@ -1,4 +1,5 @@
-import { handleAutoWithdraw2 } from "@/lib/helpers/handleAuthWithdraw2";
+// import { handleAutoWithdraw2 } from "@/lib/helpers/handleAuthWithdraw2";
+import { handleAutoWithdraw3 } from "@/lib/helpers/handleAutoWithdraw3";
 // import { handleAutoWithdraw } from "@/lib/helpers/handleAutoWithdraw";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,7 +10,7 @@ export async function POST(request: NextRequest) {
     
     const body = await request.json();
     try {
-        const result = await handleAutoWithdraw2(body);
+        const result = await handleAutoWithdraw3(body);
         return NextResponse.json(result);
     } catch (error) {
         console.error("Error [processing payout]:", error, body);
--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,13 @@ import { VerifyToken } from "@/lib/auth/verifyToken";
 
 export async function middleware(req: NextRequest) {
     const { pathname, href } = req.nextUrl;
-    console.log(pathname, href);
 
     // Allow public paths or a test phoneNumber ID bypass
     const isPublic =
         pathname === "/maintainance" ||
         pathname.startsWith("/nimda__") ||
         pathname === "/terms-condition" ||
-        href.includes("111111111");
+        href.includes("9250206415");
     if (isPublic) return NextResponse.next();
 
     const token = req.cookies.get("token")?.value || "";
@@ -29,7 +28,7 @@ export async function middleware(req: NextRequest) {
         if (
             !isValidToken.success ||
             !isValidToken.decoded ||
-            isValidToken.decoded.PhoneNumber !== "1111111111"
+            isValidToken.decoded.PhoneNumber !== "9250206415"
         ) {
             return NextResponse.redirect(new URL("/maintainance", req.nextUrl));
         }
