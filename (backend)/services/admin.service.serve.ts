91f405714d7956b3c0219e5d7ebccb02fdbd95ed
I-Vishal-kumar <vishal2983kumar@gmail.com>
2025-10-31 23:30:25 +0530

    fix overflow issue

--- (backend)/services/admin.service.serve.ts
+++ (backend)/services/admin.service.serve.ts
@@ -302,7 +302,7 @@ export const ad_getDepositTransactions = async ({
 
 const REFERAL_PERCENT = 8 / 100;
 const FIRST_DEPOSIT_BONUS_PERCENT = 10 / 100;
-const DEFAULT_DEPOSIT_BONUS_PERCENT = 5 / 100;
+// const DEFAULT_DEPOSIT_BONUS_PERCENT = 5 / 100;
 
 export const ad_settleDeposit = async (
     editedDetails: TransactionObjType
